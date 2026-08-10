49a2f5284f161048676c9f38d0c3a8ac4e993111
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-08-10 17:54:27 +0300

    test: provide FlowLy dependencies in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flowly/main.dart';
+import 'package:flowly/services/player_service.dart';
+import 'package:flowly/services/theme_service.dart';
 
 void main() {
   testWidgets('FlowLy launches', (tester) async {
-    await tester.pumpWidget(const FlowLyApp());
+    final player = PlayerService();
+    final themes = ThemeService();
+
+    await tester.pumpWidget(FlowLyApp(player: player, themes: themes));
+    await tester.pump();
+
     expect(find.text('FlowLy'), findsOneWidget);
     expect(find.text('Недавно прослушано'), findsOneWidget);
+
+    await player.dispose();
   });
 }
