fe42c2ef81cf062702526818de475d67888d2682
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-08-16 16:51:33 +0300

    fix: keep main navigation visible under player sheet

--- lib/features/mini_player/player_sheet_layout.dart
+++ lib/features/mini_player/player_sheet_layout.dart
@@ -6,6 +6,7 @@ class PlayerSheetLayout {
     required this.screenSize,
     required this.topSafeArea,
     required this.bottomSafeArea,
+    this.bottomOffset = 0,
     this.miniHeight = 72,
     this.miniArtworkSize = 48,
     this.expandedArtworkFactor = 0.78,
@@ -16,6 +17,7 @@ class PlayerSheetLayout {
   final Size screenSize;
   final double topSafeArea;
   final double bottomSafeArea;
+  final double bottomOffset;
   final double miniHeight;
   final double miniArtworkSize;
   final double expandedArtworkFactor;
@@ -33,37 +35,40 @@ class PlayerSheetLayout {
   double get miniOpacity => 1 - interval(0.0, 0.2);
   double get expandedOpacity => interval(0.8, 1.0);
 
-  double get expandedArtworkSize =>
-      screenSize.width * expandedArtworkFactor;
+  double get expandedArtworkSize => screenSize.width * expandedArtworkFactor;
 
-  double get artworkSize =>
-      lerp(miniArtworkSize, expandedArtworkSize);
+  double get artworkSize => lerp(miniArtworkSize, expandedArtworkSize);
 
   double get expandedArtworkLeft =>
       (screenSize.width - expandedArtworkSize) / 2;
 
-  double get artworkLeft =>
-      lerp(horizontalPadding, expandedArtworkLeft);
+  double get artworkLeft => lerp(horizontalPadding, expandedArtworkLeft);
 
   double get artworkTop => lerp(
         (miniHeight - miniArtworkSize) / 2,
-        topSafeArea + 28,
+        topSafeArea + 20,
       );
 
-  double get artworkRadius => lerp(10, 20);
+  double get artworkRadius => lerp(10, 18);
 
-  double get sheetHeight => lerp(miniHeight, screenSize.height);
-  double get sheetTop => screenSize.height - sheetHeight;
+  double get availableHeight =>
+      (screenSize.height - bottomOffset).clamp(0.0, screenSize.height);
+
+  double get sheetHeight => lerp(miniHeight, availableHeight);
+
+  double get sheetTop => screenSize.height - bottomOffset - sheetHeight;
 
   double get miniTitleLeft => horizontalPadding + miniArtworkSize + 12;
 
   double get miniControlsRight => horizontalPadding;
 
-  double get fullContentTop => artworkTop + artworkSize + 24;
+  double get fullContentTop => artworkTop + artworkSize + 18;
+
+  double get actionRowTop => fullContentTop + 92;
 
   double get progressBottom => bottomSafeArea + 118;
 
   double get controlsBottom => bottomSafeArea + 28;
 
-  double get scrimOpacity => interval(0.35, 0.85) * 0.35;
+  double get scrimOpacity => interval(0.35, 0.85) * 0.28;
 }
