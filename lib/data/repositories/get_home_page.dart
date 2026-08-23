2aa532b0ebfefd7d9fc44b955375ecb980e6cbaa
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-08-23 13:18:03 +0300

    migrate home repository dependencies

--- lib/data/repositories/get_home_page.dart
+++ lib/data/repositories/get_home_page.dart
@@ -1,10 +1,10 @@
 import 'dart:convert';
 
 import 'package:http/http.dart';
-import '../api/url.dart';
-import '../methods/get_response.dart';
-import '../models/song_model.dart';
-import '../models/user.dart';
+import 'package:flowly/core/network/url.dart';
+import 'package:flowly/core/utils/get_response.dart';
+import 'package:flowly/domain/entities/song_model.dart';
+import 'package:flowly/domain/entities/user.dart';
 
 class GetHomePage {
   Future<List<User>> getUsers() async {
