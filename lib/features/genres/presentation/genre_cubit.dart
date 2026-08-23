56a70224540330de051e447d0685ba2c67133463
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-08-23 14:16:06 +0300

    Migrate genre cubit to new architecture imports

--- lib/features/genres/presentation/genre_cubit.dart
+++ lib/features/genres/presentation/genre_cubit.dart
@@ -1,9 +1,10 @@
 import 'package:bloc/bloc.dart';
-import '../../../controllers/main_controller.dart';
-import '../../../models/loading_enum.dart';
-import '../../../models/song_model.dart';
-import '../../../models/user.dart';
-import '../../../repositories/get_genre_data.dart';
+
+import '../../../data/models/loading_enum.dart';
+import '../../../domain/entities/song_model.dart';
+import '../../../domain/entities/user.dart';
+import '../../../data/repositories/get_genre_data.dart';
+import '../../player/domain/main_controller.dart';
 
 part 'genre_state.dart';
 
@@ -11,11 +12,12 @@ class GenreCubit extends Cubit<GenreState> {
   final repo = GenreRepository();
 
   GenreCubit() : super(GenreState.initial());
+
   void init(String tag) async {
     try {
       emit(state.copyWith(status: LoadPage.loading));
-      var users = await repo.getUsers(tag);
-      var songs = await repo.getSongs(tag);
+      final users = await repo.getUsers(tag);
+      final songs = await repo.getSongs(tag);
       emit(state.copyWith(
         status: LoadPage.loaded,
         users: users,
