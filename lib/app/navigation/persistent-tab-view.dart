15f32d8ea23c8482ac88ec1c3b16bb1315ac0660
FrezzDEV <102240197+FrezzDEV@users.noreply.github.com>
2026-08-23 15:05:56 +0300

    Fix navigation style export so BottomNavSimple resolves

--- lib/app/navigation/persistent-tab-view.dart
+++ lib/app/navigation/persistent-tab-view.dart
@@ -1,9 +1,9 @@
 // ignore_for_file: file_names
 
-//Main TabView Widget
+// Main TabView Widget
 export 'persistent-tab-view.widget.dart';
 
-//Models
+// Models
 export 'models/persistent-nav-bar-scaffold.widget.dart';
 export 'models/persistent-bottom-nav-bar.widget.dart';
 export 'models/persisten-bottom-nav-item.widget.dart';
@@ -15,14 +15,13 @@ export 'models/page-route-transitions.model.dart';
 export 'models/nav-bar-essentials.model.dart';
 export 'models/route-settings.model.dart';
 
-//Utils
+// Utils
 export 'utils/functions.utils.dart';
 export 'utils/navigator-functions.utils.dart';
 
-//Animations
+// Animations
 export 'animations/animations.dart';
 
-//Neuomorphic-Card
-
-//Styles
+// Styles
 export 'nav-bar-styles/simple-bottom-nav-bar.widget.dart';
+export 'simple-bottom-nav-bar.widget.dart';
